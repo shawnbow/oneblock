4681aa79f8b5c95bf363c089e977aab73487ada6
Bao Zhen <baozhen@xiaoyezi.com>
2020-01-06 20:56:30 +0800

    update
    Change-Id: I003b7e61263fc8df623521f9469beacc810bd346

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
+import AccountView from './AccountView';
 import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppState {
+  accountName: string;
 }
+
+class App extends React.Component {
+  state: AppState = {
+    accountName: ""
+  }
+  num: number = 0;
+  fetchTimer1: NodeJS.Timeout | undefined;
+  
+
+  handleAccountChange = (e:any)=>{
+    this.setState({accountName: e.target.value});
+  }
+
+  componentDidMount() {
+    this.fetchTimer1 = setInterval(()=>{this.num ++}, 1000);
+  }
+  
+  componentWillUnmount() {
+    if (this.fetchTimer1) {
+      clearInterval(this.fetchTimer1);
+      this.fetchTimer1 = undefined;
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" placeholder="Input EOS Account" value={this.state.accountName} onChange={this.handleAccountChange} />
+          <button> Search </button>
+        </form>
+        <AccountView vars={{account:this.num.toString()}} />
+      </div>
+    );
+  }
+}
+
 
 export default App;
