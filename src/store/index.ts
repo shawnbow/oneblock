390e94f33d417e13ba17d5de85aa27290d8c7546
Bao Zhen <baozhen@xiaoyezi.com>
2020-01-10 09:23:05 +0800

    update
    Change-Id: Ib3c2c38aaae4d5f1ef3df1097a44521a1c52626f

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import reduxThunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { allReducers } from "./reducers";
 
 export const store = createStore(
   allReducers,
-  composeWithDevTools(applyMiddleware(thunk))
+  composeWithDevTools(applyMiddleware(reduxThunk))
 );
